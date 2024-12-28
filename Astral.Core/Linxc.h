b126390a7b9fad9738f3f40db313378fc5ddac2a
Linx145 <linussoon@gmail.com>
2024-12-28 14:48:52 +0800

    Added suppressGC keyword

--- Astral.Core/Linxc.h
+++ Astral.Core/Linxc.h
@@ -8,6 +8,7 @@
 #define def_delegate(name, returns, ...) typedef returns (*name)(__VA_ARGS__)
 #define BeginExports()
 #define EndExport()
+#define suppressGC
 
 #ifdef __cplusplus
 #define exportC extern "C"
