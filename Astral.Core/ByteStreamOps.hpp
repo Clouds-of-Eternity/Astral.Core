a5e62ffe44c9ddc276549b33005e6d960aaf977f
Linx145 <linussoon@gmail.com>
2025-01-20 00:40:12 +0800

    Updated ByteStreamOps

--- Astral.Core/ByteStreamOps.hpp
+++ Astral.Core/ByteStreamOps.hpp
@@ -1,5 +1,9 @@
+#pragma once
+#include "vector.hpp"
 #include "io.hpp"
 #include "assert.h"
+
+typedef collections::vector<u8> ByteStream;
 
 struct ByteStreamReader
 {
@@ -42,6 +46,45 @@ struct ByteStreamReader
 };
 struct ByteStreamWriter
 {
-    usize position;
-    
+    ByteStream bytes;
+
+    inline ByteStreamWriter()
+    {
+        bytes = ByteStream();
+    }
+    inline ByteStreamWriter(IAllocator allocator)
+    {
+        bytes = ByteStream(allocator);
+    }
+    template<typename T>
+    inline void Write(T instance)
+    {
+        bytes.EnsureArrayCapacity(bytes.count + sizeof(T));
+        *((T *)&bytes.ptr[bytes.count]) = instance;
+        bytes.count += sizeof(T);
+    }
+    inline void WriteArray(const void *data, usize length)
+    {
+        bytes.EnsureArrayCapacity(bytes.count + length);
+        memcpy(bytes.ptr + bytes.count, data, length);
+        bytes.count += length;
+    }
+    inline void WriteByte(u8 byte)
+    {
+        bytes.Add(byte);
+    }
+    inline void WriteEmpty(usize length)
+    {
+        bytes.EnsureArrayCapacity(bytes.count + length);
+        memset(bytes.ptr + bytes.count, 0, length);
+        bytes.count += length;
+    }
+    inline void Clear()
+    {
+        bytes.Clear();
+    }
+    inline void deinit()
+    {
+        bytes.deinit();
+    }
 };
