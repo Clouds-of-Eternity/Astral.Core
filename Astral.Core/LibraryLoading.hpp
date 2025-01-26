8c025ffb0b65d5e2192a552b11b84d090f912f0d
Linx145 <linussoon@gmail.com>
2025-01-26 16:25:53 +0800

    Removed modules.hpp, replaced and updated to LibraryLoading.hpp

---
+++ Astral.Core/LibraryLoading.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include "string.hpp"
+
+#ifdef WINDOWS
+#define WIN32_LEAN_AND_MEAN
+#define STR(s) L ## s
+#define CH(c) L ## c
+#define DIR_SEPARATOR L'\\'
+
+#define string_compare wcscmp
+#include <Windows.h>
+
+inline void *LoadMod(const char_t *path)
+{
+    HMODULE h = LoadLibraryW(path);
+    assert(h != NULL);
+    return (void*)h;
+}
+inline void *GetFuncPointer(void *h, const char *name)
+{
+    void *f = GetProcAddress((HMODULE)h, name);
+    assert(f != NULL);
+    return f;
+}
+inline void UnloadMod(void* library)
+{
+    FreeLibrary((HMODULE)library);
+}
+#else
+#include <dlfcn.h>
+#include <limits.h>
+#define STR(s) s
+#define CH(c) c
+#define DIR_SEPARATOR '/'
+#define MAX_PATH PATH_MAX
+
+#define string_compare strcmp
+
+inline void *LoadMod(const char_t *path)
+{
+    void *h = dlopen(path, RTLD_LAZY | RTLD_LOCAL);
+    assert(h != NULL);
+    return h;
+}
+inline void *GetFuncPointer(void *h, const char *name)
+{
+    void *f = dlsym(h, name);
+    assert(f != NULL);
+    return f;
+}
+inline void UnloadMod(void* library)
+{
+    dlclose(library);
+}
+
+#endif
