d845eef2983453b39e17279e3d17f8e83fae9fb1
Linx145 <32388592+Linx145@users.noreply.github.com>
2026-03-11 23:58:44 +0800

    Added pure C versions of some of the commonly used types

---
+++ Astral.Base/Allocator.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Common.h"
+
+#define KiB_SIZE 1024
+#define MiB_SIZE (KiB_SIZE * 1024)
+
+def_delegate(allocFunc, void *, void *, size_t);
+def_delegate(freeFunc, void, void *, void *);
+
+typedef struct
+{
+    void* instance;
+    allocFunc allocFunction;
+    freeFunc freeFunction;
+} IAllocator;
+
+#ifndef NO_LIBC
+
+#include <stdlib.h>
+inline void* CAllocator_Allocate(void* instance, size_t bytes)
+{
+    return malloc(bytes);
+}
+inline void CAllocator_Free(void* instance, void* ptr)
+{
+    free(ptr);
+}
+
+inline IAllocator GetCAllocator()
+{
+    IAllocator result = {NULL, &CAllocator_Allocate, &CAllocator_Free};
+}
+#endif
+
+
+inline IAllocator IAllocator_Make(void *instance, allocFunc allocateFunc, freeFunc freeFunc)
+{
+    IAllocator result = {instance, allocateFunc, freeFunc};
+    return result;
+}
+inline void *IAllocator_Allocate(IAllocator self, size_t bytes)
+{
+    return self.allocFunction(self.instance, bytes);
+}
+inline void IAllocator_Free(IAllocator self, void *memory)
+{
+    self.freeFunction(self.instance, memory);
+}
---
+++ Astral.Base/Hashmap.h
@@ -0,0 +1,243 @@
+#pragma once
+#include "List.h"
+
+#ifndef HASHMAP_MAX_WEIGHT
+#define HASHMAP_MAX_WEIGHT 0.8f
+#endif
+
+#ifndef foreach
+#define foreach(instance, iterator) for (void *instance = HashMapIterator_Next(&iterator); !iterator.completed; instance = HashMapIterator_Next(&iterator))
+#endif
+
+def_delegate(MapHashFunc, uint32_t, const void *);
+def_delegate(MapEqlFunc, bool, const void *, const void *);
+
+typedef struct
+{
+    bool initialized;
+    List entries;
+} HashMapBucket;
+
+inline HashMapBucket HashMapBucket_Create(IAllocator allocator, size_t keySize, size_t valueSize)
+{
+    HashMapBucket result;
+    result.initialized = false;
+    result.entries = List_Create(allocator, keySize + valueSize);
+    return result;
+}
+
+typedef struct
+{
+    IAllocator allocator;
+
+    MapHashFunc hashFunc;
+    MapEqlFunc eqlFunc;
+
+    HashMapBucket *buckets;
+    size_t bucketsCount;
+    size_t filledBuckets;
+    size_t count;
+
+    size_t keySize;
+    size_t valueSize;
+} HashMap;
+
+inline HashMap HashMap_Empty()
+{
+    HashMap result = {};
+    return result;
+}
+inline HashMap HashMap_Create(size_t keySize, size_t valueSize, IAllocator allocator, MapHashFunc hashFunc, MapEqlFunc eqlFunc)
+{
+    HashMap result = {allocator, hashFunc, eqlFunc, NULL, 32, 0, 0, keySize, valueSize};
+    result.buckets = IAllocator_Allocate(allocator, sizeof(HashMapBucket) * result.bucketsCount);
+    for (size_t i = 0; i < result.bucketsCount; i++)
+    {
+        result.buckets[i] = HashMapBucket_Create(allocator, keySize, valueSize);
+    }
+    return result;
+}
+
+inline void HashMap_Deinit(HashMap *self)
+{
+    if (self->buckets != NULL)
+    {
+        for (size_t i = 0; i < self->bucketsCount; i++)
+        {
+            if (self->buckets[i].initialized)
+            {
+                List_Deinit(&self->buckets[i].entries);
+            }
+        }
+        IAllocator_Free(self->allocator, self->buckets);
+    }
+}
+inline void HashMap_Clear(HashMap *self)
+{
+    if (self->buckets != NULL)
+    {
+        for (size_t i = 0; i < self->bucketsCount; i++)
+        {
+            if (self->buckets[i].initialized)
+            {
+                List_Clear(&self->buckets[i].entries);
+                //buckets[i].entries.Clear();
+            }
+        }
+        self->count = 0;
+    }
+}
+inline void HashMap_EnsureCapacity(HashMap *self)
+{
+    if (self->filledBuckets + 1.0f >= self->bucketsCount * HASHMAP_MAX_WEIGHT)
+    {
+        size_t newSize = self->bucketsCount * 2;
+
+        HashMapBucket *newBuckets = (HashMapBucket*)IAllocator_Allocate(self->allocator, newSize * sizeof(HashMapBucket));
+
+        for (size_t i = 0; i < newSize; i++)
+        {
+            newBuckets[i] = HashMapBucket_Create(self->allocator, self->keySize, self->valueSize);//Bucket(this->allocator);
+        }
+        for (size_t i = 0; i < self->bucketsCount; i++)
+        {
+            if (self->buckets[i].initialized)
+            {
+                for (size_t j = 0; j < self->buckets[i].entries.count; j++)
+                {
+                    const void *key = List_Get(&self->buckets[i].entries, j);
+                    
+                    uint32_t newKeyHash = self->hashFunc(key);
+                    size_t newIndex = newKeyHash % newSize;
+                    newBuckets[newIndex].initialized = true;
+                    //should copy over both key and value
+                    List_Add(&newBuckets[newIndex].entries, key);
+                }
+                List_Deinit(&self->buckets[i].entries);
+            }
+        }
+
+        IAllocator_Free(self->allocator, self->buckets);
+        self->buckets = newBuckets;
+        self->bucketsCount = newSize;
+    }
+}
+
+void *HashMap_Add(HashMap *self, const void *key, const void *value)
+{
+    HashMap_EnsureCapacity(self);
+
+    uint32_t hash = hashFunc(key);
+    size_t index = hash % self->bucketsCount;
+
+    if (!self->buckets[index].initialized)
+    {
+        self->buckets[index].initialized = true;
+
+        self->filledBuckets++;
+    }
+    for (size_t i = 0; i < self->buckets[index].entries.count; i++)
+    {
+        void *keyAt = List_Get(&self->buckets[index].entries, i);
+        if (eqlFunc(keyAt, key))
+        {
+            memcpy(keyAt, key, self->keySize);
+
+            void *result = (uint8_t *)keyAt + self->keySize;
+            memcpy(result, value, self->valueSize);
+            //buckets[index].entries.Get(i)->value = value;
+            return result;
+        }
+    }
+    self->count++;
+
+    const size_t entryIndex = self->buckets[index].entries.count;
+    List_AddEmpty(&self->buckets[index].entries);
+
+    void *keyOffset = (uint8_t *)self->buckets[index].entries.ptr + entryIndex * (self->keySize + self->valueSize);
+    void *valueOffset = (uint8_t *)keyOffset + self->keySize;
+    memcpy(keyOffset, key, self->keySize);
+    memcpy(valueOffset, value, self->valueSize);
+
+    return valueOffset;
+}
+
+bool HashMap_Remove(HashMap *self, const void *key)
+{
+    uint32_t hash = self->hashFunc(key);
+    size_t index = hash % self->bucketsCount;
+
+    if (self->buckets[index].initialized)
+    {
+        for (size_t i = 0; i < self->buckets[index].entries.count; i++)
+        {
+            void *keyAt = List_Get(&self->buckets[index].entries, i);
+            if (eqlFunc(keyAt, key))
+            {
+                //buckets[index].entries.RemoveAt_Swap(i);
+                List_RemoveAtSwap(&self->buckets[index].entries, i);
+
+                self->count--;
+
+                return true;
+            }
+        }
+    }
+    return false;
+}
+void *HashMap_Get(HashMap *self, const void *key)
+{
+    uint32_t hash = self->hashFunc(key);
+    size_t index = hash % self->bucketsCount;
+
+    if (self->buckets[index].initialized)
+    {
+        for (size_t i = 0; i < self->buckets[index].entries.count; i++)
+        {
+            void *keyAt = List_Get(&self->buckets[index].entries, i);
+            if (eqlFunc(keyAt, key))
+            {
+                void *valueAt = (uint8_t *)keyAt + self->keySize;
+                return valueAt;
+            }
+        }
+    }
+    return NULL;
+}
+void *HashMap_Contains(HashMap *self, const void *key)
+{
+    return HashMap_Get(self, key) != NULL;
+}
+
+typedef struct
+{
+    HashMap *map;
+    size_t i;
+    size_t j;
+    bool completed;
+} HashMapIterator;
+
+void *HashMapIterator_Next(HashMapIterator *self)
+{
+    if (self->i >= self->map->bucketsCount || self->completed)
+    {
+        return NULL;
+    }
+    while (!self->map->buckets[self->i].initialized || self->j >= self->map->buckets[self->i].entries.count)
+    {
+        self->i++;
+        if (self->i >= self->map->bucketsCount)
+        {
+            self->completed = true;
+            return NULL;
+        }
+        if (self->map->buckets[self->i].initialized && self->map->buckets[self->i].entries.count > 0)
+        {
+            self->j = 0;
+            break;
+        }
+    }
+    void *entry = List_Get(&self->map->buckets[self->i].entries, self->j);
+    self->j += 1;
+    return entry;
+}
