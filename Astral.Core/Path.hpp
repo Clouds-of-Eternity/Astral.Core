91ea85836ab3e5cbfd020a832e476f48ccdfce90
Lukaru <leonidastee1@gmail.com>
2026-05-19 10:17:12 +0800

    Fixed Path.hpp GetFileName returning the preceding directory separator

--- Astral.Core/Path.hpp
+++ Astral.Core/Path.hpp
@@ -136,7 +136,7 @@ namespace path
         {
             if (path[i] == '/' || path[i] == '\\')
             {
-                actualLastIndex = i;
+                actualLastIndex = i + 1;
             }
         }
 
