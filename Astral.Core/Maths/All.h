f16e5a22ee5dc834db48bee2cc54ca4f817e9ec0
Linx145 <32388592+Linx145@users.noreply.github.com>
2026-08-08 20:30:57 +0800

    Updated cpp_properties json, changes Maths/All.h to Maths/All.hpp

--- Astral.Core/Maths/All.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Matrix4x4.hpp"
-#include "Vec2.hpp"
-#include "Point2.hpp"
-#include "Vec3.hpp"
-#include "Vec4.hpp"
-#include "Quaternion.hpp"
-#include "Util.hpp"
-#include "Rectangle.hpp"
---
+++ Astral.Core/Maths/All.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "Matrix4x4.hpp"
+#include "Vec4.hpp"
+#include "Point2.hpp"
+#include "Rectangle.hpp"
+#include "Quaternion.hpp"
+#include "Util.hpp"
