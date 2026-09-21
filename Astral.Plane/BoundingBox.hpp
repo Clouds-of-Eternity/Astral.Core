5d5f462eedc530712686764a66101cb98be98cee
Linx145 <32388592+Linx145@users.noreply.github.com>
2026-09-21 12:34:37 +0800

    Added more functionality to BoundingBox.hpp

--- Astral.Plane/BoundingBox.hpp
+++ Astral.Plane/BoundingBox.hpp
@@ -6,13 +6,49 @@ struct BoundingBox
     Maths::Vec3 center;
     Maths::Vec3 extents;
 
-    inline Maths::Vec3 Min() const
+    inline Maths::Vec3 GetMin() const
     {
         return center - extents;
     }
-    inline Maths::Vec3 Max() const
+    inline Maths::Vec3 GetMax() const
     {
         return center + extents;
+    }
+    inline Maths::Vec3 GetTotalSize() const
+    {
+        return extents * 2.0f;
+    }
+    inline void SetTotalSize(Maths::Vec3 sz)
+    {
+        extents = sz * 0.5f;
+    }
+    inline float GetWidth() const
+    {
+        return extents.X * 2.0f;
+    }
+    inline float GetHeight() const
+    {
+        return extents.Y * 2.0f;
+    }
+    inline float GetDepth() const
+    {
+        return extents.Z * 2.0f;
+    }
+    inline void SetWidth(float value)
+    {
+        extents.X = value * 0.5f;
+    }
+    inline void SetHeight(float value)
+    {
+        extents.Y = value * 0.5f;
+    }
+    inline void SetDepth(float value)
+    {
+        extents.Z = value * 0.5f;
+    }
+    inline bool Contains(Maths::Vec3 position) const
+    {
+        return position < GetMax() && position > GetMin();
     }
     inline bool Intersects(BoundingBox other) const
     {
